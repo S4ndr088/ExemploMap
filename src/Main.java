a6b2f8afa1523a6322fd00d273b4c71eb1ebf4be
Sandro Roberto Ventura <sandrorv88@gmail.com>
2023-10-05 17:24:09 -0300

    Ajuste para contabilizar o votos.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -9,14 +11,29 @@ public class Main {
         System.out.print("Informe o diretorio do arquivo: ");
         String path = sc.next();
 
+        Map<String, Integer> votos = new HashMap<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String linha = br.readLine();
 
             while (linha != null){
-                System.out.println(linha);
+                String[] retorno = linha.split(",");
+                String nome = retorno[0];
+                Integer contador = Integer.valueOf(retorno[1]);
+
+                if (votos.containsKey(nome)){
+                    Integer votosAtual = votos.get(nome);
+                    votos.put(nome, contador + votosAtual);
+                } else {
+                    votos.put(nome, contador);
+                }
 
                 linha = br.readLine();
             }
+
+            votos.entrySet().stream()
+                    .forEach(x -> System.out.printf("%s : %s%n", x.getKey(), x.getValue()));
+
         } catch (IOException e){
             System.out.println("Erro: " + e.getMessage());
         }
