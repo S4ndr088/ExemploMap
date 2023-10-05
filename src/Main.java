cf5c5c3d649e5bd4509910a9c5c8540deb2bae7e
Sandro Roberto Ventura <sandrorv88@gmail.com>
2023-10-05 16:54:56 -0300

    Add arquivo registros. Implementado estrutura de leitura do arquivo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe o diretorio do arquivo: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String linha = br.readLine();
+
+            while (linha != null){
+                System.out.println(linha);
+
+                linha = br.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
